a02422c58deeb151b4afc0faf0c54e439a18347d
MichealRollins-Green <micheal.rollins.green@gmail.com>
2017-05-24 12:33:48 -0400

    Add files via upload

--- Driver-ASE/Driver_ASE_Lib/Driver_ASE_Scripts/2.0_Prep_For_Bad_SNPs_CNVs.pl
+++ Driver-ASE/Driver_ASE_Lib/Driver_ASE_Scripts/2.0_Prep_For_Bad_SNPs_CNVs.pl
@@ -142,7 +142,7 @@ $Bad_SNP_CNV->pull_normal("cnv_file_names.txt","normal_cnv.txt");
 #move barcode from the cds_sorted directory into a file called have_cd
 print "Now running mv_bcode for cds_sorted_dir\n";
 `ls $RNA_Path/cds_sorted > cds_sorted_dir`;
-mv_bcode(file containing list of bed files from the cds_sorted directory,output file)
+#mv_bcode(file containing list of bed files from the cds_sorted directory,output file)
 $Bad_SNP_CNV->mv_bcode("cds_sorted_dir","have_cd");
 
 print "Now running hv_cd\n";
